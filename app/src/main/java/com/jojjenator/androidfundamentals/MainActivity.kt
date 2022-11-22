a472f73fcc1670da26fad785111525789f044a07
nasvalljohan <j.nasvall95@gmail.com>
2022-11-22 23:48:07 +0100

    Toast and context

--- app/src/main/java/com/jojjenator/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/jojjenator/androidfundamentals/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.jojjenator.androidfundamentals
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 // Inherits from AppCompatActivity, a class that describes what an activity is.
@@ -8,11 +10,22 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     // OnCreate is inherited from AppCompatActivity, IE entrypoint for this activity and any other created activity.
-    //CTRL + O to see whats overrideable, what is available from AppCompatActivity.
+    // CTRL + O to see whats overrideable, what is available from AppCompatActivity.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        val btnShowToast = findViewById<Button>(R.id.btnShowToast)
+
+        // Context provided "this" is ok because activities are subclasses to context class
+        // Activity-context only lives as long as the activity does
+        // - if you have a singleton object that needs the context and u need it to live as long as
+        // - the application does then you should use app-context, if you use activity-context
+        // - memory leak might occur.
+        // Application-context lives for as long as the application.
+        btnShowToast.setOnClickListener {
+            Toast.makeText(this, "Hello!", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onPause() {
