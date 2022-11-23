a1aed16f68d1cecfed9f48f2f677c2daa52f1b26
nasvalljohan <j.nasvall95@gmail.com>
2022-11-23 14:08:53 +0100

    Implicit intents
    Adding locally stored image to imageview

--- app/src/main/java/com/jojjenator/androidfundamentals/ThirdActivity.kt
+++ app/src/main/java/com/jojjenator/androidfundamentals/ThirdActivity.kt
@@ -1,11 +1,38 @@
 package com.jojjenator.androidfundamentals
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class ThirdActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        val takePhoto = findViewById<Button>(R.id.btnGetImage)
+        val imageView = findViewById<ImageView>(R.id.ivPhoto)
+
+        // getContent gets called within intent. ser imageview to uri if its not null.
+        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            if (uri != null){
+                imageView.setImageURI(uri)
+            }
+        }
+
+        // Implicit intent, earlier only used explicit intent
+        takePhoto.setOnClickListener {
+
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                // looks for type image/* (* == all), can specify image/jpeg or image/png etc
+                getContent.launch("image/*")
+
+            }
+        }
     }
+
 }
