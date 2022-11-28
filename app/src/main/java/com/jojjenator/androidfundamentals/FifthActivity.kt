123c990f4b8ca0f09d2dc205f990d551369baa54
nasvalljohan <j.nasvall95@gmail.com>
2022-11-28 12:03:26 +0100

    TabLayoutMediator with ViewPager

--- app/src/main/java/com/jojjenator/androidfundamentals/FifthActivity.kt
+++ app/src/main/java/com/jojjenator/androidfundamentals/FifthActivity.kt
@@ -2,8 +2,11 @@ package com.jojjenator.androidfundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.jojjenator.androidfundamentals.adapter.ViewPagerAdapter
 
 class FifthActivity : AppCompatActivity() {
@@ -28,12 +31,38 @@ class FifthActivity : AppCompatActivity() {
         viewPager.adapter = adapter
 
         // This changes the scrolling orientation
-        viewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
+        //viewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
 
         // With these you can simulate drags without doing it to provide helpful UX.
-        viewPager.beginFakeDrag()
-        viewPager.fakeDragBy(-10f)
-        viewPager.endFakeDrag()
+        //viewPager.beginFakeDrag()
+        //viewPager.fakeDragBy(-10f)
+        //viewPager.endFakeDrag()
+
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayoutPagerItems)
+
+        // Connect tabLayout with the existing viewPager.
+        // Instead of .setUpWithViewPager which is deprecated
+
+        // Lambda function, takes two params -> we called them tab and position.
+        TabLayoutMediator(tabLayout, viewPager){tab, position ->
+            tab.text = "Tab ${position + 1}"
+        }.attach() // -> to attach this mediator to the views.
+
+        // How to add listener for selected tab:
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@FifthActivity, "Selected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@FifthActivity, "UnSelected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@FifthActivity, "Reselected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+        })
+
 
 
 
