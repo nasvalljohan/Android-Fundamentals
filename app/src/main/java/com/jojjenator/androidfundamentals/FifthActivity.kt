554d140dd16f8c0ed94b14fed46f2006ed752eed
nasvalljohan <j.nasvall95@gmail.com>
2022-11-28 13:38:26 +0100

    NavigationDrawer

--- app/src/main/java/com/jojjenator/androidfundamentals/FifthActivity.kt
+++ app/src/main/java/com/jojjenator/androidfundamentals/FifthActivity.kt
@@ -2,20 +2,28 @@ package com.jojjenator.androidfundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
-import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.jojjenator.androidfundamentals.adapter.ViewPagerAdapter
 
 class FifthActivity : AppCompatActivity() {
+
+    // lateinit is a promise. we promise to init this later.
+    // benefit of this is that variable is global in class.
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fifth)
 
         val imageList = listOf(
-            R.drawable.asset_2snuslogo,
+            R.drawable.playstore_snuslogo,
             R.drawable.playstore1,
             R.drawable.playstore2,
             R.drawable.playstore3,
@@ -63,8 +71,42 @@ class FifthActivity : AppCompatActivity() {
             }
         })
 
+        // Navigation drawer:
+        var drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
 
+        // Here we init out lateinit toggle
+        // 1st param activity, 2nd param layout, 3rd and 4th are used for accessibility, e.g. for blind people.
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
 
+        // Add toggle to our layout
+        drawerLayout.addDrawerListener(toggle)
 
+        // After its connected we need to tell our toggle that is should sync its state, we tell it its now ready to be used.
+        toggle.syncState()
+
+        // When we toggle, the toggle button moves to a arrow button - open and close drawer.
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        var navView = findViewById<NavigationView>(R.id.navView)
+        navView.setNavigationItemSelectedListener {
+            // it == the current menu item that was clicked
+            when(it.itemId){
+                R.id.miItem1 -> Toast.makeText(applicationContext, "clicked item 1", Toast.LENGTH_SHORT).show()
+                R.id.miItem2 -> Toast.makeText(applicationContext, "clicked item 2", Toast.LENGTH_SHORT).show()
+                R.id.miItem3 -> Toast.makeText(applicationContext, "clicked item 3", Toast.LENGTH_SHORT).show()
+            }
+            // true == return out of lambda application
+            true
+        }
+    }
+
+    // This is where we need our toggle-variable to be global, its used in new override function.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        // if user clicked on toggle-button
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
